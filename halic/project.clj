679ed2b39a1890cdcf9f19bfc61a6d0a53017171
kaosbeat <kasper.jordaens@gmail.com>
2014-04-18 22:25:01 +0200

    working overtone test
    no open sound control (yet) shader tone not working

--- halic/project.clj
+++ halic/project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.9.1"]
+                 [shadertone "0.2.3"]
+;;                  [overtone/osc-clj "0.8.1"]
+;;                  [polynome "0.2.2"] ;;for monome
+                 ]
   :main ^:skip-aot halic.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- halic/src/halic/core.clj
+++ halic/src/halic/core.clj
@@ -1,7 +1,33 @@
 (ns halic.core
-  (:gen-class))
+  (:gen-class)
+)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(ns halic.overtone
+  (:use [overtone.live]
+;;         [overtone.osc]
+        )
+  )
+;;   (:require [shadertone.tone :as t])
+
+
+;; (def PORT 4242)
+
+;; ; start a server and create a client to talk with it
+;; (def server (osc-server PORT))
+;; (def client (osc-client "localhost" PORT))
+
+
+
+(definst bar [freq 220] (saw 110))
+(definst baz [freq 440] (* 0.3 (saw freq)))
+(definst quux [freq 440] (* 0.3 (saw freq)))
+
+
+(definst tem [freq 440 depth 10 rate 6 length 3]
+    (* 0.3
+       (line:kr 0.5 1 length FREE)
+       (saw (+ freq (* depth (sin-osc:kr rate))))))
+
+(tem 400 4 6 3)
+
+;; (stop)
