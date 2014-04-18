9f0e1e12aaba2be2e62aa4b4447c8638c3a20f11
kaosbeat <kasper.jordaens@gmail.com>
2014-04-18 23:10:32 +0200

    some toying with the shader

--- halic/shaders/disco.glsl
+++ halic/shaders/disco.glsl
@@ -17,9 +17,10 @@ void main(void)
     float v1 = 0.1 + 5.0*iOvertoneVolume;
     float v2 = 0.1 + 0.5*hz(800);
     float v3 = 0.1 + 0.5*hz(8000);
-    vec3 col = vec3(0.0, 0.0, 0.0);
+
+    vec3 col = vec3(0.1, 0.0, 0.4);
     float v1x = uv.x + sin(5.0*iGlobalTime + 1.5*uv.y)*v1;
-    float v2x = uv.x + 0.5 + sin(3.0*iGlobalTime + 0.8*uv.y)*v1;
+    float v2x = uv.x + 0.5 + sin(30.0*iGlobalTime + 0.8*uv.y)*v1;
     float v3x = uv.x - 0.5 + sin(7.0*iGlobalTime + 3.2*uv.y)*v1;
     col += vec3(1.0,0.0,1.0) * abs(0.066/v1x) * v1;
     col += vec3(1.0,1.0,0.0) * abs(0.066/v2x) * v2;
@@ -32,4 +33,7 @@ void main(void)
     col += vec3(1.0,1.0,1.0) * a1;
 
     gl_FragColor = vec4(col,1.0);
+
+
+
 }
