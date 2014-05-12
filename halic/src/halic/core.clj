1a3f093b9f812948409336bd3f2bb9ab03615ca1
kaosbeat <kasper.jordaens@gmail.com>
2014-05-12 20:48:58 +0200

    testing OSC over network!

--- halic/src/halic/core.clj
+++ halic/src/halic/core.clj
@@ -3,8 +3,7 @@
 ;; )
 
 (ns halic.core
-  (:use [overtone.live]
-;;         [overtone.osc]
+  (:use [overtone.osc]
         )
    (:require [quil.snippet :refer [defsnippet]]
             [quil.core :refer :all])
@@ -15,25 +14,40 @@
 
 ; start a server and create a client to talk with it
 (def server (osc-server PORT))
-(def client (osc-client "localhost" PORT))
-
-(sc-osc-debug-off)
+(def client (osc-client "192.168.7.104" PORT))
 
 
-(definst bar [freq 220] (saw 110))
-(definst baz [freq 440] (* 0.3 (saw freq)))
-(definst quux [freq 440] (* 0.3 (saw freq)))
 
 
-(definst tem [freq 440 depth 10 rate 6 length 3]
-    (* 0.3
-       (line:kr 0.5 1 length FREE)
-       (saw (+ freq (* depth (sin-osc:kr rate))))))
+(sc-osc-debug-on)
 
-(tem 400 4 6 3)
-(tem 200 4 6 3)
-;; (stop)
 
-(t/start "shaders/disco.glsl"
-         :width 800 :height 800
-         :title "Quasicrystal")
+; send it some messages
+(doseq [val (range 10)]
+
+
+;; (osc-send)
+;; (osc-msg)
+(osc-handle server "/test" (fn [msg] (println "MSG: " msg)))
+
+
+;;   [overtone.live]
+;; (definst bar [freq 220] (saw 110))
+;; (definst baz [freq 440] (* 0.3 (saw freq)))
+;; (definst quux [freq 440] (* 0.3 (saw freq)))
+
+
+;; (definst tem [freq 440 depth 10 rate 6 length 3]
+;;     (* 0.3
+;;        (line:kr 0.5 1 length FREE)
+;;        (saw (+ freq (* depth (sin-osc:kr rate))))))
+
+;; (tem 400 4 6 3)
+;; (tem 200 4 6 3)
+;; ;; (stop)
+
+;; (t/start "shaders/disco.glsl"
+;;          :width 800 :height 800
+;;          :title "Quasicrystal")
+
+
