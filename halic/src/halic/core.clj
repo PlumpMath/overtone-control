ca619a83049195a041140fc852e3dc0c5b64c144
kaosbeat <kasper.jordaens@gmail.com>
2014-05-12 21:15:31 +0200

    Revert ac6443f..f55d7db
    This rolls back to commit ac6443feb939fb63cf7e89b0369d75c5caf53d03.

--- halic/src/halic/core.clj
+++ halic/src/halic/core.clj
@@ -3,7 +3,8 @@
 ;; )
 
 (ns halic.core
-  (:use [overtone.osc]
+  (:use [overtone.live]
+;;         [overtone.osc]
         )
    (:require [quil.snippet :refer [defsnippet]]
             [quil.core :refer :all])
@@ -14,40 +15,25 @@
 
 ; start a server and create a client to talk with it
 (def server (osc-server PORT))
-(def client (osc-client "192.168.7.104" PORT))
+(def client (osc-client "localhost" PORT))
+
+(sc-osc-debug-off)
 
 
+(definst bar [freq 220] (saw 110))
+(definst baz [freq 440] (* 0.3 (saw freq)))
+(definst quux [freq 440] (* 0.3 (saw freq)))
 
 
-(sc-osc-debug-on)
+(definst tem [freq 440 depth 10 rate 6 length 3]
+    (* 0.3
+       (line:kr 0.5 1 length FREE)
+       (saw (+ freq (* depth (sin-osc:kr rate))))))
 
+(tem 400 4 6 3)
+(tem 200 4 6 3)
+;; (stop)
 
-; send it some messages
-(doseq [val (range 10)]
-
-
-;; (osc-send)
-;; (osc-msg)
-(osc-handle server "/test" (fn [msg] (println "MSG: " msg)))
-
-
-;;   [overtone.live]
-;; (definst bar [freq 220] (saw 110))
-;; (definst baz [freq 440] (* 0.3 (saw freq)))
-;; (definst quux [freq 440] (* 0.3 (saw freq)))
-
-
-;; (definst tem [freq 440 depth 10 rate 6 length 3]
-;;     (* 0.3
-;;        (line:kr 0.5 1 length FREE)
-;;        (saw (+ freq (* depth (sin-osc:kr rate))))))
-
-;; (tem 400 4 6 3)
-;; (tem 200 4 6 3)
-;; ;; (stop)
-
-;; (t/start "shaders/disco.glsl"
-;;          :width 800 :height 800
-;;          :title "Quasicrystal")
-
-
+(t/start "shaders/disco.glsl"
+         :width 800 :height 800
+         :title "Quasicrystal")
