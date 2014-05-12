5115393c8491cddcee3ab9cf48930671770f7998
kaosbeat <kasper.jordaens@gmail.com>
2014-05-12 21:31:10 +0200

    solved namespace troubled, but it's harder to to use overtone

--- halic/src/halic/core.clj
+++ halic/src/halic/core.clj
@@ -3,37 +3,35 @@
 ;; )
 
 (ns halic.core
-  (:use [overtone.live]
-;;         [overtone.osc]
-        )
-   (:require [quil.snippet :refer [defsnippet]]
-            [quil.core :refer :all])
+;;   (:refer-clojure :exclude [tan])
+  (:require [overtone.live :as live])
+  (:require [overtone.osc :as osc])
+;;    (:require [quil.snippet :refer [defsnippet]])
+  (:require [quil.core :refer :all])
+
 ;;   (:require [shadertone.tone :as t])
 )
+
 
 (def PORT 4242)
 
 ; start a server and create a client to talk with it
-(def server (osc-server PORT))
-(def client (osc-client "localhost" PORT))
+(def server (osc/osc-server PORT))
+(def client (osc/osc-client "localhost" PORT))
 
-(sc-osc-debug-off)
-
-
-(definst bar [freq 220] (saw 110))
-(definst baz [freq 440] (* 0.3 (saw freq)))
-(definst quux [freq 440] (* 0.3 (saw freq)))
+(live/sc-osc-debug-off)
 
 
-(definst tem [freq 440 depth 10 rate 6 length 3]
+
+(live/definst tem [freq 440 depth 10 rate 6 length 3]
     (* 0.3
-       (line:kr 0.5 1 length FREE)
-       (saw (+ freq (* depth (sin-osc:kr rate))))))
+       (live/line:kr 0.5 1 length live/FREE)
+       (live/saw (+ freq (* depth (live/sin-osc:kr rate))))))
 
 (tem 400 4 6 3)
 (tem 200 4 6 3)
 ;; (stop)
 
-(t/start "shaders/disco.glsl"
-         :width 800 :height 800
-         :title "Quasicrystal")
+;; (t/start "shaders/disco.glsl"
+;;          :width 800 :height 800
+;;          :title "Quasicrystal")
