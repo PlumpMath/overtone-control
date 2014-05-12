099fa5cf74594d83073cfa3db650af5b485a4572
kaosbeat <kasper.jordaens@gmail.com>
2014-05-12 22:03:21 +0200

    starting to get the 3Dstuff

--- halic/src/halic/core.clj
+++ halic/src/halic/core.clj
@@ -6,8 +6,10 @@
 ;;   (:refer-clojure :exclude [tan])
   (:require [overtone.live :as live])
   (:require [overtone.osc :as osc])
-;;    (:require [quil.snippet :refer [defsnippet]])
+;;   (:require [quil.snippet :refer [defsnippet]])
   (:require [quil.core :refer :all])
+  (:require [quil.helpers.seqs :refer [range-incl]])
+  (:require [quil.helpers.drawing :refer [line-join-points]])
 
 ;;   (:require [shadertone.tone :as t])
 )
@@ -35,3 +37,39 @@
 ;; (t/start "shaders/disco.glsl"
 ;;          :width 800 :height 800
 ;;          :title "Quasicrystal")
+
+(defn rand-y
+  [border-y]
+  (+ border-y (rand (- (height) (* 2 border-y)))))
+
+(defn setup []
+  (background 255)
+
+  (smooth)
+  (stroke 0 500)
+  (line 20 50 480 50)
+  (stroke 2 50 70)
+)
+
+
+
+(defn draw []
+  (background 255)
+  (stroke-weight 2)
+  (let [step      10
+        border-x  30
+        border-y  10
+        xs        (range-incl border-x (- (width) border-x) step)
+        ys        (repeatedly #(rand-y border-y))
+        line-args (line-join-points xs ys)]
+    (dorun (map #(apply line %) line-args))))
+
+
+(defsketch example-5
+  :title "Random Scribble"
+  :setup setup
+  :draw draw
+  :size [500 100])
+
+
+
