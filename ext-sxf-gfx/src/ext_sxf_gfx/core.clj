0299bc857b97c7c34ad991262832ee55e0efac9c
kaosbeat <kasper.jordaens@gmail.com>
2014-10-13 16:52:19 +0200

    going external!

--- ext-sxf-gfx/src/ext_sxf_gfx/core.clj
+++ ext-sxf-gfx/src/ext_sxf_gfx/core.clj
@@ -2,20 +2,10 @@
   (:use [overtone.core]  ;;if this is on, will use external server!!!
        ))
 
-(connect-external-server 57110)
+(connect-external-server "192.168.0.3" 57110)
 
 
 
 ;; gfx helpers
 
-(defn show-frame-rate [options]
-  (let [; retrieve existing draw function or use empty one if not present
-        draw (:draw options (fn []))
-        ; updated-draw will replace draw
-        updated-draw (fn []
-                       (draw) ; call user-provided draw function
-                       (q/fill 0)
-                       (q/text-num (q/current-frame-rate) 10 10))]
-    ; set updated-draw as :draw function
-    (assoc options :draw updated-draw)))
 
