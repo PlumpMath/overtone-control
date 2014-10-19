07fe14b2ea79506c375490756df8ef0b0bbc2a72
kaosbeat <kasper.jordaens@gmail.com>
2014-10-19 21:44:41 +0200

    sending beat the oldskool way (OSC)

--- ext-sxf-gfx/src/ext_sxf_gfx/ext-gfx-tap-example.clj
+++ ext-sxf-gfx/src/ext_sxf_gfx/ext-gfx-tap-example.clj
@@ -1,12 +1,33 @@
 (ns ext-sxf-gfx.core
+  (:require [overtone.osc :as osc])
   (:require [quil.core :as q]
             [quil.middleware :as m]
             [quil.helpers.seqs :refer [seq->stream range-incl cycle-between]]
 ))
 
+;;bidirectional comms
+(def GrPORT 4242)
+;; ; start a server and create a client to talk with it
+(def Gserver (osc/osc-server GrPORT))
+;; (osc/osc-close Gserver)
+(def GsPORT 4243)
+(def Gclient (osc/osc-client "localhost" GsPORT))
+
+(osc/osc-handle Gserver "/beat" (fn [msg]
+;;                                 (println (:args msg))
+                                 (reset! beat {:beatnum (first (:args msg))})
+                                ))
+
+(osc/osc-handle Gserver "/vvv/a" (fn [msg]
+                                (println (:args msg))
+;;                                  (reset! beat {:beatnum (first (:args msg))})
+                                ))
+
+
+
   @(:a ext-sxf-gfx.core/mytaps)
   @(:mod-env ext-sxf-gfx.core/qfm)
-  (:taps ext-sxf-gfx.core/v)
+  (:taps ext-sxf-gfx.core/vvv)
   (int @(get (:taps ext-sxf-gfx.core/v) :a))
 
 (defn setup []
@@ -19,6 +40,7 @@
   {
    :qa  (- (int @(get (:taps ext-sxf-gfx.core/v) :a)) 255)
    :qb  (- (int @(get (:taps ext-sxf-gfx.core/v) :b)) 255)
+;;    :qmod-env (@(get (:taps ext-sfx-gfx.core/qfm) :mod-env))
     })
 
 (defn draw [state]
--- ext-sxf-gfx/src/ext_sxf_gfx/ext-sfx-tap-example.clj
+++ ext-sxf-gfx/src/ext_sxf_gfx/ext-sfx-tap-example.clj
@@ -2,30 +2,73 @@
 ;; connect to the project using this namespace
   )
 
+;;    _____ ____  __  __ __  __  _____
+;;   / ____/ __ \|  \/  |  \/  |/ ____|
+;;  | |   | |  | | \  / | \  / | (___
+;;  | |   | |  | | |\/| | |\/| |\___ \
+;;  | |___| |__| | |  | | |  | |____) |
+;;   \_____\____/|_|  |_|_|  |_|_____/
+
+
+
+
+;;bidirectional comms
+(def SrPORT 4243)
+;; ; start a server and create a client to talk with it
+(def Sserver (osc/osc-server SrPORT))
+;; (osc/osc-close Sserver)
+(def SsPORT 4242)
+(def Sclient (osc/osc-client "localhost" SsPORT))
+
+
+
+
+;; send the beat out!
+(def beat (atom {:beatnum 0}))
+(def metro (metronome 200))
+;; for sending the beatnumber over to quil!
+(defn beat2quil [nome ]
+    (let [beat (nome)]
+        (apply-by (nome (inc beat)) (osc-send client "/beat" beat))
+        (apply-by (nome (inc beat)) beat2quil nome [])
+      ))
+
+(beat2quil metro)
+(:beatnum @beat)
+
+;;    _______     ___   _ _______ _    _  _____
+;;   / ____\ \   / / \ | |__   __| |  | |/ ____|
+;;  | (___  \ \_/ /|  \| |  | |  | |__| | (___
+;;   \___ \  \   / | . ` |  | |  |  __  |\___ \
+;;   ____) |  | |  | |\  |  | |  | |  | |____) |
+;;  |_____/   |_|  |_| \_|  |_|  |_|  |_|_____/
+
+
+
+
 ;; define a synth
 (defsynth vvv
   []
   (let [a (+ 300 (* 50 (sin-osc:kr (/ 1 3))))
         b (+ 300 (* 100 (sin-osc:kr (/ 1 5))))
-        _ (tap :a 60 (a2k a))
-        _ (tap :b 60 (a2k b))]
+        _ (tap:kr :a 60 (a2k a))
+        _ (tap:kr :b 60 (a2k b))]
     (out 0 (pan2 (+ (sin-osc a)
                     (sin-osc b))))))
 
 ;; run an instance of the synth
 
-;; (vvv)
+(vvv)
 ;; kill it if you have to
 (kill vvv)
 
 
 (def v (vvv))
-
-(vvv)
-
+@(get-in v [:taps :a])
 (kill v)
 
-v
+
+
 ;; start tapping
 
   (def mytaps (:taps (vvv)))
@@ -36,31 +79,42 @@ v
 
 
 
-;;   (defsynth fm [carrier 880 divisor 5.0 depth 4.0 out-bus 0]
-;;   (let [modulator (/ carrier divisor)
-;;         mod-env (env-gen (lin 1.9 3.8 -2.8))
-;;         amp-env (env-gen (lin 1 0 4.0 1) :action FREE)
-;;         filt (rlpf (+ carrier modulator ) 100 0.1)
-;;         _ (tap :mod-env 30 mod-env)]
-;;     (out out-bus  (pan2 (* 0.15 amp-env
+  (defsynth fm [carrier 880 divisor 5.0 depth 4.0 out-bus 0]
+  (let [modulator (/ carrier divisor)
+        mod-env (env-gen (lin 1.9 3.8 -2.8))
+        amp-env (env-gen (lin 1 0 4.0 1) :action FREE)
+        filt (rlpf (+ carrier modulator ) 100 0.1)
+        _ (tap :mod-env 30 mod-env)]
+    (out out-bus  (pan2 (* 0.15 amp-env
+                          (sin-osc (+ carrier
+                                      (* mod-env (* carrier depth) (sin-osc modulator))))))
+;;          (tap :pan :kr (pan2 (* 0.15 amp-env
 ;;                           (sin-osc (+ carrier
-;;                                       (* mod-env (* carrier depth) (sin-osc modulator))))))
-;; ;;          (tap :pan :kr (pan2 (* 0.15 amp-env
-;; ;;                           (sin-osc (+ carrier
-;; ;;                                       (* mod-env (* carrier depth) (sin-osc modulator)))))))
-;;     )))
+;;                                       (* mod-env (* carrier depth) (sin-osc modulator)))))))
+    )))
 
-;; @(:mod-env (:taps (fm)))
+@(:mod-env (:taps (fm)))
 
-;; (def qfm (fm 300 0 10 10))
-;; qfm
+(def qfm (fm 300 0 10 10))
+qfm
 
 ;; (get (:taps (qfm)))
+@(get-in qfm [:taps :mod-env])
 
 ;; @(:mod-env qfm )
 
 ;; (def qfm (:taps (fm 300 0 10 10)))
 ;; @(:mod-env qfm )
+
+
+;;    _____ ______ ____
+;;   / ____|  ____/ __ \
+;;  | (___ | |__ | |  | |___
+;;   \___ \|  __|| |  | / __|
+;;   ____) | |___| |__| \__ \
+;;  |_____/|______\___\_\___/
+
+
 
 ;; (def pats {
 ;;            fm  [1 0 0 0 0 0 0 0]
