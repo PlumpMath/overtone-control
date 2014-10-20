49fa28219c9ff346a53644210b8a9385b44c274c
kaosbeat <kasper.jordaens@gmail.com>
2014-10-21 01:13:39 +0200

    slides outlines

--- ext-sxf-gfx/src/ext_sxf_gfx/ext-gfx-talk-DODghent2014.clj
+++ ext-sxf-gfx/src/ext_sxf_gfx/ext-gfx-talk-DODghent2014.clj
@@ -100,12 +100,16 @@
 
 ;; === HALIC ===
   (q/fill 255 255 0)
+  (fitimage "resources/control_room.jpg")
   (q/text-size 50)
   (q/text "H.euristic" 100 100)
   (q/text "AL.gorithmic" 100 200)
   (q/text "I.nteractive" 100 300)
-  (q/text "C.ontrollers" 100 400)
-  (def key words ["humans", "machines", "interaction"])
+  (q/text "C.omputing" 100 400)
+;;   (def keywords ["humans", "machines", "interaction"])
+;; Ralph McQuarrie concept art for the original Battlestar Galactica series
+
+
 ;; ;; while explaining halic, make some sound, and map a tap on the color, showing the interactive part
 
 ;; ==== H ====
@@ -114,14 +118,13 @@
 ;;   (q/text-size 500)
 ;;   (q/text "H" 100 600)
 
-;; === humans: kaosbeat & bohrbug ===
+;; ;; === humans: kaosbeat & bohrbug ===
 ;;   (fitimage "resources/halic.jpg")
 
 ;; = kaosbeat =
 ;; (fitimage "resources/kaos.png")
 ;; (heading "Hi There!")
 ;; (twittername "@kaosbeat")
-
 ;; (def keywords ["Architect" "@kaosbeat" "creative technologist"])
 
 ;; = bohrbug =
@@ -129,24 +132,33 @@
 ;; (def keywords ["describe" "yourself" "in" "keywords" ])
 
 ;; ==== AL ====
-;;   stands for alien
+;;   stands for alien / algorithmic
 
+;; "for $50 you can buy a home PC program that will crush most grandmasters."
 ;;   (fitimage "resources/Fincollo.jpg")
 ;;   (imgcredit "http://culprittech.blogspot.be/")
 ;;   (q/text-size 400)
 ;; ;;   (q/text "AL" 50 550)
 ;;   (def keywords (["alien" "data" "unknown/untrusted source" "machines"]))
-
+;;   (def keywords ["goldorak" "ijzeren reus"])
 
 ;; === philosophy ===
 ;;   (fitimage "resources/control_room.jpg")
 ;;   (heading "Control the galaxy")
 
 
-;; ;; === livecoding ===
+;; === livecoding ===
 ;; (fitimage "resources/moog.jpg")
 ;; (def keywords ["live" "tool" "or" "instrument"])
-;;
+
+
+;; === example ===
+;;  ==sine wave ==
+
+  ;; draw (def function already loaded)
+  ;; sound (sin-osc 440 with tap)
+
+  ;;viz and sound
 
 
 
@@ -155,6 +167,60 @@
 ;; (def keywords ["lambda-calculus" "functional programming"])
 
   )
+
+
+
+
+=== lambdasonic promo ===
+=== halic === (D + K)
+stands for
+halic is ons project.
+Heuristic, Algorithmic, interactive, computing
+a search for music and sound synesthesia
+
+=== humans ===
+kaosbeat/bohrbug (D + K)
+
+=== why even consider humans, they are weak === (K)
+Kasparov => human + computers = good
+
+=== HAL is not (good) enough === (K)
+it's not intuition
+aesthaetics
+
+=== HAL is not interactive computation (K)
+computer as an instrument, not a tool
+deep blue is not an instrument, but a tool
+
+=== the moog is an instrument (compare) (K -> D)
+interactive music making, analog live coding
+
+=== waarom dan geen moog? computers kunnenm nog net iets meer?  (D)
+geluid = heel goed, maar compositie was nog pover
+
+=== composing? -> symbolic composer (D)
+not good enough (geen improvisatie)
+
+=== composing and improvising with machines (D)
+
+=== tools (K)
+Pure data / max -> Lighttable / Emacs
+dichter bij de machine
+
+=== because bytecode, because JVM inject, lambda-calculus
+
+
+=== go R
+
+
+
+
+
+
+
+
+
+
 
 
 
