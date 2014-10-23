00bfb4d7494264a0cc26f087c9d529ef3dfeb111
kaosbeat <kasper.jordaens@gmail.com>
2014-10-23 07:20:08 +0200

    added newtonian

--- dodhalic/src/dodhalic/gfxcore.clj
+++ dodhalic/src/dodhalic/gfxcore.clj
@@ -2,23 +2,25 @@
   (:require [overtone.osc :as osc])
   (:require [quil.core :as q]
             [quil.middleware :as m]
-;;             [newtonian.particle-system :as newt]
-;;             [newtonian.utils :as utils]
-;;             [quil.helpers.calc :refer [mul-add]]
+            [newtonian.particle-system :as newt]
+            [newtonian.utils :as utils]
+            [quil.helpers.calc :refer [mul-add]]
             [quil.helpers.seqs :refer [seq->stream range-incl cycle-between steps]]
             [quil.helpers.drawing :refer [line-join-points]]
             )
-;;   (:import newtonian.utils.Vector2D)
+  (:import newtonian.utils.Vector2D)
             )
 
 
-(def width 594)
-(def height 840)
 
-(defn setup []
-  (q/frame-rate 30)
 
-)
+;;    _____ ____  __  __ __  __  _____
+;;   / ____/ __ \|  \/  |  \/  |/ ____|
+;;  | |   | |  | | \  / | \  / | (___
+;;  | |   | |  | | |\/| | |\/| |\___ \
+;;  | |___| |__| | |  | | |  | |____) |
+;;   \_____\____/|_|  |_|_|  |_|_____/
+
 
 
 ;;bidirectional comms
@@ -47,3 +49,45 @@
 ;;                                 (println (:args msg))
                                  (reset! beat {:beatnum (first (:args msg))})
                                 ))
+
+;;    ____  _    _ _____ _            _          __  __
+;;   / __ \| |  | |_   _| |          | |        / _|/ _|
+;;  | |  | | |  | | | | | |       ___| |_ _   _| |_| |_
+;;  | |  | | |  | | | | | |      / __| __| | | |  _|  _|
+;;  | |__| | |__| |_| |_| |____  \__ \ |_| |_| | | | |
+;;   \___\_\\____/|_____|______| |___/\__|\__,_|_| |_|
+
+
+(def width 594)
+(def height 840)
+
+
+
+(defn setup []
+  (q/frame-rate 30)
+
+)
+
+
+(defn update [state]
+  {; Update sketch state by changing circle color and position.
+  ;;   {:color (mod (+ (:color state) 0.7) 255)
+;; ;;   {:color (* 255 (get (get-taps) :left))
+;;    :angle (+ (:angle state) 0.1)
+;; ;;    :size (* 500 (get (get-taps) :left))
+;;    :c100 (c100)
+    })
+
+(defn draw [state])
+
+(q/defsketch tapdemo
+  :title "lambdasonic"
+  :size [width height]
+  :setup setup
+  :update update
+  :draw draw
+  :renderer :p3d
+  :middleware [m/fun-mode]
+
+  )
+
--- dodhalic/src/dodhalic/gfxlive.clj
+++ dodhalic/src/dodhalic/gfxlive.clj
@@ -1,1 +1,79 @@
 (ns dodhalic.gfxcore)
+
+(mod4)
+(newt/add-field (Vector2D. 280.0 305.0) 100.0)
+(newt/add-emitter (Vector2D. 200.0 280.0) (Vector2D. 0.0 10.0))
+
+(swap! newt/fields pop)
+(swap! newt/emitters pop)
+(def test (atom ()))
+
+(swap! test conj :blaaah)
+
+(swap! test pop)
+test
+
+newt/emitters
+(nth @newt/emitters 0)
+(swap! (get (nth @newt/emitters 0) :position) :x 200)
+ (get (nth @newt/emitters 0) :position)
+newt/fields
+
+
+(swap! newt/emitters (nth @newt/emitters 0)  )
+;; (swap!)
+;; (assoc-in @newt/emitters [0] {:position{:x 200}}))
+
+@newt/draw-options
+newt/draw-options
+
+(swap! newt/draw-options assoc :accelerations false)
+
+
+
+(swap!(nth @newt/emitters 0) assoc-in {:position {:x 200}})
+
+(defn update [state]
+  ;;update statemap here
+  (newt/add-new-particles)
+  (newt/update-particles width height)
+  {})
+
+(defn draw-particle [{:keys [position velocity accel]}]
+  (let [x (:x position)
+        y (:y position)
+        a (utils/mag accel)
+        s (utils/mag velocity)]
+;; (    println s)
+;;     (q/no-stroke)
+
+;;     (q/fill-int (q/color 150 (/ (* 220 0.5) s) (* 76 s) 255))
+    (q/fill 255 255 0)
+    (q/ellipse x y 2 23.0 )
+;;     (q/line x y (+ x 200) (* (+ y 200) (mod4)))
+    )
+  )
+
+(defn draw-field [{:keys [position]}]
+  (let [x (:x position)
+        y (:y position)]
+    (q/no-stroke)
+    (q/fill-int (q/color 255 (* (mod4) 64) 64))
+    (q/ellipse x y 10.0 10.0)))
+
+
+
+(defn draw [state]
+  (q/background 28)
+;;   draw stuff here
+  (q/line 420 209 342 239)
+    (let [particles @newt/particles
+        fields @newt/fields]
+   (doseq [p particles]
+     (draw-particle p))
+   (doseq [f fields]
+     (draw-field f)))
+
+  )
+
+(rest '(100))
